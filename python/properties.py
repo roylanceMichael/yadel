2e68b41ed32ef0b71c9fc141bd202433f2df008e
Mike Roylance <roylance.michael@gmail.com>
2016-10-05 17:49:06 -0600

    updated with latest version of yaclib

---
+++ python/properties.py
@@ -0,0 +1,12 @@
+#generated from yaclib on Wed Oct 05 17:43:01 MDT 2016
+#Wed Oct 05 17:43:01 MDT 2016
+name="api"
+fullName="org_roylance_yadel_api"
+author="roylance.michael@gmail.com"
+license="MIT"
+minor="121"
+major="0"
+fullVersion="0.121"
+githubUrl="https\://github.com/roylanceMichael/yadel.git"
+description="models to interface with the org.roylance.yadel.api system"
+group="org.roylance.yadel"
--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
+import properties
+
 
 setup(
-    name="org_roylance_yadel_api",
-    version="0.117",
-    author="roylance.michael@gmail.com",
-    license='MIT',
+    name=properties.fullName,
+    version=properties.fullVersion,
+    author=properties.author,
+    license=properties.license,
     include_package_data=True,
     install_requires=['protobuf==3.0.0'],
-    description="models to interface with the org.roylance.yadel.api system",
-    author_email="roylance.michael@gmail.com",
-    url="https://github.com/roylanceMichael/yadel.git",
+    description=properties.description,
+    author_email=properties.author,
+    url=properties.githubUrl,
     packages=find_packages(exclude=['tests']))
