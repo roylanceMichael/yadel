4b6d3cdd6652817b71847a07c7ab938db044d130
Mike Roylance <roylance.michael@gmail.com>
2016-11-17 12:20:46 -0700

    updated the plugin a bit more

--- plugin/src/main/groovy/org/roylance/yadel/YadelServerPlugin.groovy
+++ plugin/src/main/groovy/org/roylance/yadel/YadelServerPlugin.groovy
@@ -7,9 +7,14 @@ import org.roylance.yaclib.core.utilities.FileProcessUtilities
 
 class YadelServerPlugin extends DefaultTask {
     public String currentProjectName;
-    public String projectName;
-    public String serverVersion;
-    public String seedHost;
+
+    public String projectName = ""
+    public String serverUrl = ""
+    public String serverVersion = ""
+
+    public String awsAutoScalingGroupName = ""
+    public String fullServerTarUrl = ""
+
 
     @TaskAction
     def packageServer() {
@@ -21,18 +26,37 @@ class YadelServerPlugin extends DefaultTask {
             println("yadel project not listed, exiting...")
             return
         }
-        if (seedHost == null || seedHost.size() == 0) {
-            println("no seedHost listed, exiting...")
+        if (serverUrl == null || serverUrl.size() == 0) {
+            println("no serverUrl listed, exiting...")
+            return
+        }
+        if (serverVersion == null || serverVersion.size() == 0) {
+            println("no serverVersion listed, exiting...")
+            return
+        }
+        if (awsAutoScalingGroupName == null || awsAutoScalingGroupName.size() == 0) {
+            println("no awsAutoScalingGroupName listed, exiting...")
+            return
+        }
+        if (fullServerTarUrl == null || fullServerTarUrl.size() == 0) {
+            println("no fullServerTarUrl listed, exiting...")
             return
         }
 
         new File("build").delete()
 
         def properties = new Properties()
-        def stream = new FileInputStream("../" + projectName + "/gradle.properties")
+        def propertiesFile = new File("../" + projectName + "/gradle.properties")
+        def stream = new FileInputStream(propertiesFile)
         properties.load(stream)
-        properties.setProperty("seedHost", seedHost)
+        properties.setProperty("serverUrl_", serverUrl)
+        properties.setProperty("awsAutoScalingGroupName_", awsAutoScalingGroupName)
+        properties.setProperty("fullServerTarUrl_", fullServerTarUrl)
         stream.close()
+
+        def outputStream = new FileOutputStream(propertiesFile)
+        properties.store(outputStream, "set serverUrl_: ${serverUrl}, awsAutoScalingGroupName_: ${awsAutoScalingGroupName}, fullServerTarUrl_: ${fullServerTarUrl}")
+        outputStream.close()
 
         FileProcessUtilities.INSTANCE.executeProcess("../" + projectName, "gradle", "clean")
         FileProcessUtilities.INSTANCE.executeProcess("../" + projectName, "gradle", "tarApp")
--- python/properties.py
+++ python/properties.py
@@ -1,10 +1,10 @@
-#generated from yaclib on Wed Nov 16 17:29:32 MST 2016
-#Wed Nov 16 17:29:32 MST 2016
+#generated from yaclib on Thu Nov 17 12:05:10 MST 2016
+#Thu Nov 17 12:05:10 MST 2016
 major="0"
 name="api"
 githubUrl="https\://github.com/roylanceMichael/yadel.git"
 author="roylance.michael@gmail.com"
 description="models to interface with the org.roylance.yadel.api system"
 license="MIT"
-minor="153"
+minor="156"
 group="org.roylance.yadel"
