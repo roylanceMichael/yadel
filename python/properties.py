2106b993901d676dbde1ea3ad5bfaf08c7a369b2
Mike Roylance <roylance.michael@gmail.com>
2016-11-29 20:38:14 -0700

    dependency update

--- python/properties.py
+++ python/properties.py
@@ -1,10 +1,10 @@
-#generated from yaclib on Tue Nov 29 11:02:24 MST 2016
-#Tue Nov 29 11:02:24 MST 2016
+#generated from yaclib on Tue Nov 29 20:25:24 MST 2016
+#Tue Nov 29 20:25:24 MST 2016
 major="0"
 name="api"
 githubUrl="https\://github.com/roylanceMichael/yadel.git"
 author="roylance.michael@gmail.com"
 description="models to interface with the org.roylance.yadel.api system"
 license="MIT"
-minor="170"
+minor="171"
 group="org.roylance.yadel"
