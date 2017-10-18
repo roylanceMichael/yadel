1872d08dfce61f22fbcecf15f47d052035287ddf
Mike Roylance <roylance.michael@gmail.com>
2017-10-17 23:56:03 -0600

    latest changes

--- yadelpy/properties.py
+++ yadelpy/properties.py
@@ -1,10 +1,10 @@
-#generated from yaclib on Tue Oct 17 23:02:36 MDT 2017
-#Tue Oct 17 23:02:36 MDT 2017
+#generated from yaclib on Tue Oct 17 23:52:21 MDT 2017
+#Tue Oct 17 23:52:21 MDT 2017
 major="0"
 name="yadel"
 githubUrl="https\://github.com/roylanceMichael/yadel.git"
 author="roylance.michael@gmail.com"
 description="models to interface with the org.roylance.yadel system"
 license="MIT"
-minor="216"
+minor="217"
 group="org.roylance"
